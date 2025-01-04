7aac43c058aabde33bcf4be29923b351aad1ba6a
Daniel Mellado <mellado.m2@gmail.com>
2025-01-04 21:31:03 +0100

    feat: add dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout() {
+  return (
+    <>
+      <header>
+        <h3>header</h3>
+      </header>
+      <aside>
+        <p>aside</p>
+      </aside>
+      <main>main</main>
+    </>
+  );
+}
