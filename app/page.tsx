34d16bd6dec2606faf27d9df3ca9dc85054c832e
Daniel Mellado <mellado.m2@gmail.com>
2025-02-27 18:59:26 +0100

    fix login responsive

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
-        <h1 className="text-center text-xl">Soldi</h1>
+      <main className="flex flex-col gap-8 row-start-2 items-center w-full">
+        <h1 className="text-center text-4xl">Soldi</h1>
         <SignIn />
       </main>
     </div>
