a30da52d90960e2c0f9039ce79a8c2dd4e20f929
Daniel Mellado <mellado.m2@gmail.com>
2026-01-05 20:13:11 +0100

    Update turso-client.ts

--- utils/turso-client.ts
+++ utils/turso-client.ts
@@ -2,12 +2,12 @@ import { type Client, createClient } from "@libsql/client/http";
 import "dotenv/config";
 
 export function tursoClient(): Client {
-  const url = process.env.TURSO_PRE_URL?.trim();
+  const url = process.env.TURSO_DATABASE_PROD_URL?.trim();
   if (url === undefined) {
     throw new Error("TURSO_URL is not defined");
   }
 
-  const authToken = process.env.TURSO_PRE_TOKEN?.trim();
+  const authToken = process.env.TURSO_AUTH_PROD_TOKEN?.trim();
   if (authToken === undefined) {
     if (!url.includes("file:")) {
       throw new Error("TURSO_URL is not defined");
@@ -15,7 +15,7 @@ export function tursoClient(): Client {
   }
 
   return createClient({
-    url: process.env.TURSO_PRE_URL!,
-    authToken: process.env.TURSO_PRE_TOKEN!,
+    url: process.env.TURSO_DATABASE_PROD_URL!,
+    authToken: process.env.TURSO_AUTH_PROD_TOKEN!,
   });
 }
